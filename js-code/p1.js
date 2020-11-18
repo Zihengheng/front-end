1c2bd14e8fe41cc84cd8b16aff73b7ce9b3e3d53
wzh <wzh@wangzihengdeMacBook-Air.local>
2020-11-18 14:27:28 +0800

    update

---
+++ js-code/p1.js
@@ -0,0 +1,36 @@
+// toString method
+var a = 255;
+a = a.toString();
+
+console.log(a);
+console.log(typeof a);
+
+//String method
+ var b = null;
+ String(b);
+ console.log(b);
+ 
+ //Number 函数
+var c = 233;
+var d = false;
+var e = "abcd";
+
+// function toNumber(i){
+//     Number(i);
+//     console.log(`转换成数字为：${i}`);
+// }
+// toNumber(c);
+// toNumber(d);
+// toNumber(e);
+
+console.log(Number(e));
+
+//逻辑运算符
+const a1 = "wangzihenghaha"
+a1 && console.log('看a1看不出来');
+
+const a2 = undefined;
+a2 && console.log("看a2看不出来");
+
+//练习1
+
